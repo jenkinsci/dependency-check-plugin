c40a89e0ccf64c5860fcc240c4448930c8de05a9
stevespringett <steve@springett.us>
2017-05-30 21:33:52 -0500

    Added initial support for publishing results to Dependency-Track v3.
    https://github.com/stevespringett/dependency-track/issues/52
    
    JENKINS-44570

--- src/main/java/org/jenkinsci/plugins/DependencyCheck/DependencyCheckPlugin.java
+++ src/main/java/org/jenkinsci/plugins/DependencyCheck/DependencyCheckPlugin.java
@@ -26,6 +26,7 @@ import hudson.plugins.analysis.views.DetailFactory;
 public class DependencyCheckPlugin extends Plugin {
 
     static final String PLUGIN_NAME = "DependencyCheck";
+    static final String DT_PLUGIN_NAME = "DependencyTrack";
 
     @Override
     public void start() {
