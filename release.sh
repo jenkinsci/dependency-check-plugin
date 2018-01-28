e23ef334bee791fa81b3c0fae5e14cc5994a3802
stevespringett <steve@springett.us>
2018-01-28 11:03:35 -0600

    Updating java version to 1.8

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script (for macOS) will release the plugin to the Jenkins update site.
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export PATH=JAVA_HOME/bin:$PATH
 
 mvn clean
