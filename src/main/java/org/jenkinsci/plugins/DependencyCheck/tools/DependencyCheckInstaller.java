cb5e2ca799d8f820678a0ffb5c0c3ebf74092cd0
stevespringett <steve@springett.us>
2019-06-14 16:42:39 -0500

    Adding installation

---
+++ src/main/java/org/jenkinsci/plugins/DependencyCheck/tools/DependencyCheckInstaller.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of Dependency-Check Jenkins plugin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jenkinsci.plugins.DependencyCheck.tools;
+
+import java.io.IOException;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.tools.DownloadFromUrlInstaller;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolInstallerDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class DependencyCheckInstaller extends DownloadFromUrlInstaller {
+
+    @DataBoundConstructor
+    public DependencyCheckInstaller(String id) {
+        super(id);
+    }
+
+    @Override
+    public FilePath performInstallation(ToolInstallation tool, Node node, TaskListener log) throws IOException, InterruptedException {
+        return super.performInstallation(tool, node, log);
+    }
+
+    @Extension
+    public static final class DependencyCheckInstallerDescriptor extends ToolInstallerDescriptor<DependencyCheckInstaller> {
+
+        @Override
+        public String getDisplayName() {
+            return "Install from dl.bintray.com";
+        }
+
+        @Override
+        public boolean isApplicable(Class<? extends ToolInstallation> toolType) {
+            return toolType == DependencyCheckInstallation.class;
+        }
+    }
+}
