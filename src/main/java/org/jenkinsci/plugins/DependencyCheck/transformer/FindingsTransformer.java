aa9311c325897f50e4c837113c619616bd525034
stevespringett <steve@springett.us>
2019-06-25 15:18:31 -0500

    Ported transformer to match odc model objects

--- src/main/java/org/jenkinsci/plugins/DependencyCheck/transformer/FindingsTransformer.java
+++ src/main/java/org/jenkinsci/plugins/DependencyCheck/transformer/FindingsTransformer.java
@@ -34,106 +34,95 @@ public class FindingsTransformer {
     public JSONObject transform(List<Finding> findings) {
         final JSONArray columns = new JSONArray();
 
-        final JSONObject c1 = new JSONObject();
-        c1.put("name", "dependency.nameLabel");
-        c1.put("title", "Name");
-        c1.put("visible", true);
-        c1.put("filterable", true);
-        c1.put("sortValue", "dependency.name");
-        columns.add(c1);
+        final JSONObject fileName = new JSONObject();
+        fileName.put("name", "dependency.fileName");
+        fileName.put("title", "File Name");
+        fileName.put("visible", true);
+        fileName.put("filterable", true);
+        fileName.put("sortValue", "dependency.fileName");
+        columns.add(fileName);
 
-        final JSONObject c2 = new JSONObject();
-        c2.put("name", "dependency.versionLabel");
-        c2.put("title", "Version");
-        c2.put("visible", true);
-        c2.put("filterable", true);
-        c2.put("sortValue", "component.version");
-        columns.add(c2);
+        final JSONObject filePath = new JSONObject();
+        filePath.put("name", "dependency.filePath");
+        filePath.put("title", "File Path");
+        filePath.put("breakpoints", "all");
+        filePath.put("visible", true);
+        filePath.put("filterable", false);
+        filePath.put("sortValue", "dependency.filePath");
+        columns.add(filePath);
 
-        final JSONObject c3 = new JSONObject();
-        c3.put("name", "dependency.groupLabel");
-        c3.put("title", "Group");
-        c3.put("visible", true);
-        c3.put("filterable", true);
-        c3.put("sortValue", "component.group");
-        columns.add(c3);
+        final JSONObject sha1 = new JSONObject();
+        sha1.put("name", "dependency.sha1");
+        sha1.put("title", "SHA-1");
+        sha1.put("breakpoints", "all");
+        sha1.put("visible", true);
+        sha1.put("filterable", true);
+        sha1.put("sortValue", "dependency.sha1");
+        columns.add(sha1);
 
-        final JSONObject c4 = new JSONObject();
-        c4.put("name", "vulnerability.vulnIdLabel");
-        c4.put("title", "Vulnerability");
-        c4.put("visible", true);
-        c4.put("filterable", true);
-        c4.put("sortValue", "vulnerability.vulnId");
-        columns.add(c4);
+        final JSONObject sha256 = new JSONObject();
+        sha256.put("name", "dependency.sha256");
+        sha256.put("title", "SHA-256");
+        sha256.put("breakpoints", "all");
+        sha256.put("visible", true);
+        sha256.put("filterable", true);
+        sha256.put("sortValue", "dependency.sha256");
+        columns.add(sha256);
 
-        final JSONObject c5 = new JSONObject();
-        c5.put("name", "vulnerability.severityLabel");
-        c5.put("title", "Severity");
-        c5.put("visible", true);
-        c5.put("filterable", true);
-        c5.put("sortValue", "vulnerability.severityRank");
-        columns.add(c5);
+        final JSONObject vulnNameLabel = new JSONObject();
+        vulnNameLabel.put("name", "vulnerability.nameLabel");
+        vulnNameLabel.put("title", "Vulnerability");
+        vulnNameLabel.put("visible", true);
+        vulnNameLabel.put("filterable", true);
+        vulnNameLabel.put("sortValue", "vulnerability.name");
+        columns.add(vulnNameLabel);
 
-        final JSONObject c6 = new JSONObject();
-        c6.put("name", "vulnerability.cweLabel");
-        c6.put("title", "CWE");
-        c6.put("visible", true);
-        c6.put("filterable", true);
-        c6.put("sortValue", "vulnerability.cweId");
-        JSONObject c61 = new JSONObject();
-        c61.put("width", "30%");
-        c6.put("style", c61);
-        columns.add(c6);
+        final JSONObject severityLabel = new JSONObject();
+        severityLabel.put("name", "vulnerability.severityLabel");
+        severityLabel.put("title", "Severity");
+        severityLabel.put("visible", true);
+        severityLabel.put("filterable", true);
+        severityLabel.put("sortValue", "vulnerability.severityRank");
+        columns.add(severityLabel);
 
-        final JSONObject c7 = new JSONObject();
-        c7.put("name", "vulnerability.title");
-        c7.put("title", "Title");
-        c7.put("breakpoints", "all");
-        c7.put("visible", true);
-        c7.put("filterable", false);
-        columns.add(c7);
+        final JSONObject cwe = new JSONObject();
+        cwe.put("name", "vulnerability.cwe");
+        cwe.put("title", "CWE");
+        cwe.put("visible", true);
+        cwe.put("filterable", true);
+        cwe.put("sortValue", "vulnerability.cwe");
+        columns.add(cwe);
 
-        final JSONObject c8 = new JSONObject();
-        c8.put("name", "vulnerability.subtitle");
-        c8.put("title", "Subtitle");
-        c8.put("breakpoints", "all");
-        c8.put("visible", true);
-        c8.put("filterable", false);
-        columns.add(c8);
-
-        final JSONObject c9 = new JSONObject();
-        c9.put("name", "vulnerability.description");
-        c9.put("title", "Description");
-        c9.put("breakpoints", "all");
-        c9.put("visible", true);
-        c9.put("filterable", false);
-        columns.add(c9);
-
-        final JSONObject c10 = new JSONObject();
-        c10.put("name", "vulnerability.recommendation");
-        c10.put("title", "Recommendation");
-        c10.put("breakpoints", "all");
-        c10.put("visible", true);
-        c10.put("filterable", false);
-        columns.add(c10);
+        final JSONObject vulnDescription = new JSONObject();
+        vulnDescription.put("name", "vulnerability.description");
+        vulnDescription.put("title", "Description");
+        vulnDescription.put("breakpoints", "all");
+        vulnDescription.put("visible", true);
+        vulnDescription.put("filterable", false);
+        columns.add(vulnDescription);
 
         final JSONArray rows = new JSONArray();
         for (Finding finding: findings) {
-            final Dependency component = finding.getDependency();
+            final Dependency dependency = finding.getDependency();
             final Vulnerability vulnerability = finding.getVulnerability();
             final JSONObject row = new JSONObject();
-            row.put("component.fileName", component.getFileName());
-            row.put("component.filePath", component.getFilePath());
+            row.put("dependency.fileName", dependency.getFileName());
+            row.put("dependency.filePath", dependency.getFilePath());
+            row.put("dependency.description", dependency.getDescription());
+            row.put("dependency.license", dependency.getLicense());
+            row.put("dependency.md5", dependency.getMd5());
+            row.put("dependency.sha1", dependency.getSha1());
+            row.put("dependency.sha256", dependency.getSha256());
             row.put("vulnerability.source", vulnerability.getSource());
             row.put("vulnerability.name", vulnerability.getName());
-            //row.put("vulnerability.vulnNameLabel", generateVulnerabilityField(vulnerability.getSource(), vulnerability.getName()));
+            row.put("vulnerability.nameLabel", generateVulnerabilityField(vulnerability.getSource().name(), vulnerability.getName()));
             row.put("vulnerability.description", vulnerability.getDescription());
-            //row.put("vulnerability.severityLabel", generateSeverityField(vulnerability.getSeverity()));
-            //row.put("vulnerability.severity", vulnerability.getSeverity());
-            //row.put("vulnerability.severityRank", vulnerability.getSeverityRank());
-            //row.put("vulnerability.cweLabel", generateCweField(vulnerability.getCweId(), vulnerability.getCweName()));
-            //row.put("vulnerability.cweId", vulnerability.getCweId());
-            //row.put("vulnerability.cweName", vulnerability.getCweName());
+            row.put("vulnerability.severityLabel", generateSeverityField(Severity.normalize(vulnerability.getSeverity())));
+            row.put("vulnerability.severity", vulnerability.getSeverity());
+            row.put("vulnerability.severityRank", Severity.normalize(vulnerability.getSeverity()).ordinal());
+            if (vulnerability.getCwes() != null && !vulnerability.getCwes().isEmpty()) {
+                row.put("vulnerability.cwe", vulnerability.getCwes().get(0));
+            }
             rows.add(row);
         }
         final JSONObject data = new JSONObject();
@@ -155,13 +144,6 @@ public class FindingsTransformer {
 
     private String generateVulnerabilityField(String source, String vulnId) {
         return "<span class=\"vuln-source vuln-source-" + source.toLowerCase() + "\">" + source + "</span>" + vulnId;
-    }
-
-    private String generateCweField(Integer cweId, String cweName) {
-        if (cweId == null || cweName == null) {
-            return null;
-        }
-        return generateTruncatedStringField("CWE-" + cweId + " " + cweName);
     }
 
     private String generateTruncatedStringField(String in) {
