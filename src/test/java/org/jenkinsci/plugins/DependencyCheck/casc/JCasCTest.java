1cf1408ab97cc595c3448eb0f60a87fee99e60d0
Nikolas Falco <nfalco79@hotmail.com>
2023-03-18 16:04:46 +0100

    [JENKINS-56898] Add support for configuration-as-code
    Add unit test to prove it is compatible with CasC plugin

---
+++ src/test/java/org/jenkinsci/plugins/DependencyCheck/casc/JCasCTest.java
@@ -0,0 +1,78 @@
+/*
+ * This file is part of Dependency-Check Jenkins plugin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jenkinsci.plugins.DependencyCheck.casc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+
+import org.jenkinsci.plugins.DependencyCheck.tools.DependencyCheckInstallation;
+import org.jenkinsci.plugins.DependencyCheck.tools.DependencyCheckInstaller;
+import org.jvnet.hudson.test.RestartableJenkinsRule;
+
+import hudson.tools.InstallSourceProperty;
+import hudson.tools.ToolDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolProperty;
+import hudson.tools.ToolPropertyDescriptor;
+import hudson.util.DescribableList;
+import io.jenkins.plugins.casc.misc.RoundTripAbstractTest;
+import jenkins.model.Jenkins;
+
+public class JCasCTest extends RoundTripAbstractTest {
+
+    @Override
+    protected void assertConfiguredAsExpected(RestartableJenkinsRule restartableJenkinsRule, String s) {
+        checkInstallations(restartableJenkinsRule.j.jenkins);
+    }
+
+    private void checkInstallations(Jenkins j) {
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        final ToolDescriptor<DependencyCheckInstallation> descriptor = (ToolDescriptor) j.getDescriptor(DependencyCheckInstallation.class);
+        final ToolInstallation[] installations = descriptor.getInstallations();
+        assertThat(installations, arrayWithSize(2));
+
+        ToolInstallation withInstaller = installations[0];
+        assertEquals("latest", withInstaller.getName());
+
+        final DescribableList<ToolProperty<?>, ToolPropertyDescriptor> properties = withInstaller.getProperties();
+        assertThat(properties, hasSize(1));
+        final ToolProperty<?> property = properties.get(0);
+
+        assertThat(((InstallSourceProperty) property).installers, //
+                containsInAnyOrder( //
+                        allOf(instanceOf(DependencyCheckInstaller.class)) //
+                ));
+
+        ToolInstallation withoutInstaller = installations[1];
+        assertThat(withoutInstaller, //
+                allOf( //
+                        hasProperty("name", equalTo("anotherWithNoInstall")), //
+                        hasProperty("home", equalTo("/onePath") //
+                        )));
+    }
+
+    @Override
+    protected String stringInLogExpected() {
+        return "dependency-check";
+    }
+}
