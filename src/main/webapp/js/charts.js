b8b725e59813f9c65b34ecde66deac76ec85cdf3
Nicola Isotta <nicola.isotta@gmail.com>
2026-04-11 15:12:44 +0200

    remove tipped.js and use standard jenkins tooltips

--- src/main/webapp/js/charts.js
+++ src/main/webapp/js/charts.js
@@ -19,16 +19,16 @@ function generateSeverityDistributionBar(targetId, label, critical, high, medium
     var percentUnassigned = (unassigned > 0 ? (unassigned/(critical+high+medium+low+info+unassigned))*100 : 0);
     var block = '<span class="odc-section-label ">' + label + '</span><div class="severity-distribution">';
     if (critical === 0 && high === 0 && medium === 0 && low === 0 && info === 0 && unassigned === 0) {
-        block += '<div class="severity-distribution-bar severity-info-bg odc-tooltip" title="No Vulnerabilities Found" style="width:100%">No Vulnerabilities Found</div>';
+        block += '<div class="severity-distribution-bar severity-info-bg" tooltip="No Vulnerabilities Found" style="width:100%">No Vulnerabilities Found</div>';
     } else {
-        block += '<div class="severity-distribution-bar severity-critical-bg odc-tooltip" title="Critical: ' + critical + ' (' + Math.round(percentCritical*10)/10 + '%)" style="width:' + percentCritical+ '%">' + critical + '</div>';
-        block += '<div class="severity-distribution-bar severity-high-bg odc-tooltip" title="High: ' + high + ' (' + Math.round(percentHigh*10)/10 + '%)" style="width:' + percentHigh + '%">' + high + '</div>';
-        block += '<div class="severity-distribution-bar severity-medium-bg odc-tooltip" title="Medium: ' + medium + ' (' + Math.round(percentMedium*10)/10 + '%)" style="width:' + percentMedium + '%">' + medium + '</div>';
-        block += '<div class="severity-distribution-bar severity-low-bg odc-tooltip" title="Low: ' + low + ' (' + Math.round(percentLow*10)/10 + '%)" style="width:' + percentLow + '%">' + low + '</div>';
-        block += '<div class="severity-distribution-bar severity-info-bg odc-tooltip" title="Info: ' + info + ' (' + Math.round(percentInfo*10)/10 + '%)" style="width:' + percentInfo + '%">' + info + '</div>';
-        block += '<div class="severity-distribution-bar severity-unassigned-bg odc-tooltip" title="Unassigned: ' + unassigned + ' (' + Math.round(percentUnassigned*10)/10 + '%)" style="width:' + percentUnassigned + '%">' + unassigned + '</div>';
+        block += '<div class="severity-distribution-bar severity-critical-bg" tooltip="Critical: ' + critical + ' (' + Math.round(percentCritical*10)/10 + '%)" style="width:' + percentCritical+ '%">' + critical + '</div>';
+        block += '<div class="severity-distribution-bar severity-high-bg" tooltip="High: ' + high + ' (' + Math.round(percentHigh*10)/10 + '%)" style="width:' + percentHigh + '%">' + high + '</div>';
+        block += '<div class="severity-distribution-bar severity-medium-bg" tooltip="Medium: ' + medium + ' (' + Math.round(percentMedium*10)/10 + '%)" style="width:' + percentMedium + '%">' + medium + '</div>';
+        block += '<div class="severity-distribution-bar severity-low-bg" tooltip="Low: ' + low + ' (' + Math.round(percentLow*10)/10 + '%)" style="width:' + percentLow + '%">' + low + '</div>';
+        block += '<div class="severity-distribution-bar severity-info-bg" tooltip="Info: ' + info + ' (' + Math.round(percentInfo*10)/10 + '%)" style="width:' + percentInfo + '%">' + info + '</div>';
+        block += '<div class="severity-distribution-bar severity-unassigned-bg" tooltip="Unassigned: ' + unassigned + ' (' + Math.round(percentUnassigned*10)/10 + '%)" style="width:' + percentUnassigned + '%">' + unassigned + '</div>';
     }
     block += '</div>';
     document.getElementById(targetId).innerHTML = block;
-    Tipped.create('.odc-tooltip');
+    Behaviour.applySubtree(document.getElementById(targetId));
 }
